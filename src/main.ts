7e44aee9b9ef4058d4f65dfc02d808143044f1e5
KeepCodeing <3350680655@qq.com>
2021-08-29 20:06:38 +0800

    feature:hello接口简单集成了SwaggerApi

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(1919);
+  const app = await NestFactory.create(AppModule);  
 
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('Swagger API')
+    .setDescription('This is a project created by nestjs and Swagger')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('doc', app, document);
+
+  // 注意端口监听要写在最后面，否则前面装载的模块可能没有加载成功?
+  await app.listen(1919);
 }
 bootstrap();
--- src/modules/hello/hello.controller.ts
+++ src/modules/hello/hello.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { ApiBody, ApiHeader, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { HelloService } from './hello.service'
 
 @Controller('hello')
+@ApiTags('hello Api')
 export class HelloController {
   constructor(private readonly helloService: HelloService) {}
 
@@ -11,12 +13,17 @@ export class HelloController {
   // 在下面测试发现这玩意写了的意思不是“参数里必须要有个这个名称的变量”
   // 而是解构...也就是ES6解构和这个写法只能选一个，个人觉得ES6写法更好
   @Get()
+  @ApiQuery({ name: 'msg', required: true })
   sayHello(@Query() { msg }): string {
-    return this.helloService.say(msg);
+    return this.helloService.say({ msg });
   }
 
   // 这里Body一定要接受x-www-form-urlencode，不然无法获得参数
+  // 暂且没找到swagger怎么设置POST参数，这里这样写只能手动构造一个json数据传过去...
   @Post()
+  @ApiBody({ description: 'I am a post request' })
+  // @ApiParam({ name: 'id', required: true })
+  // @ApiParam({ name: 'name', required: true })
   addHello(@Body() { id, name }) {
     return this.helloService.add({ id, name });
   }
@@ -26,6 +33,7 @@ export class HelloController {
   // 而不是query的字符串参数
   // 另外要注意，这个动态路由的:id就对应着参数的键
   @Delete(':id')
+  @ApiParam({ name: 'id', required: true })
   deleteHello(@Param() { id }) {
     // parma，也可以直接接受整个对象
     return this.helloService.delete({ id });
@@ -33,6 +41,8 @@ export class HelloController {
 
   // 注意更新比较特殊，它是根据url获取id，根据body获取更新数据
   @Patch(':id')
+  @ApiParam({ name: 'id', required: true })
+  @ApiBody({ description: 'name body' })
   patchHello(@Param() { id }, @Body() { name }) {
     return this.helloService.update({ id, name });
   }
